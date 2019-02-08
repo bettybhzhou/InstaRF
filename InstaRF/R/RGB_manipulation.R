37d93ef4491ad1107e806b3f1cf96370c87d0da1
Betty Zhou <betty.zhou@alumni.ubc.ca>
2019-02-08 15:19:03 -0800

    "added empty functions"

---
+++ InstaRF/R/RGB_manipulation.R
@@ -0,0 +1,15 @@
+
+#' RGB manipulation
+#'
+#' @param string, path to the input png file
+#' @param string, path to the output png file
+#' @return png file in the output path
+#' @export
+#'
+#' @examples
+#' RGB_manipulation("input,png", "output.png")
+
+
+RGB_manipulation <- function() {
+  print("RGB manipulation working")
+}
